53b60183b9fdbe4a07c4e952fa25d823d5e5cd6c
SheilaRod <133283037+SheilaRod@users.noreply.github.com>
2023-06-22 01:31:29 -0300

    Add files via upload

---
+++ HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/ Reemplazar el método index:
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/ Agregar este método:
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+
+
+    }
+}
